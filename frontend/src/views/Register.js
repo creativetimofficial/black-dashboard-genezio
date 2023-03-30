3fb51553e6dc734c60d22f470cfc7c7c0000b198
Radu Dumitrescu <r.dumitrescu@reply.com>
2023-03-30 17:41:42 +0300

    typo

--- frontend/src/views/Register.js
+++ frontend/src/views/Register.js
@@ -216,7 +216,7 @@ function Register(props) {
                 type="submit"
                 onClick={e => handleSubmit(e)}
               >
-                Sumbit
+                Submit
                 {isSubmitting ? "..." : ""}
               </Button>
             </CardFooter>
